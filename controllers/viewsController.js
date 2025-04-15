3a94cca6c17b38c47cef59451df3fe04d2b95609
FaizanMateen <faizanmateen20@gmail.com>
2025-04-16 00:29:34 +0530

    last update

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
-const Booking = require('../models/bookingModel')
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking')
+    res.ocals.alert =
+      "Your booking was successful! Please check your email for a confirmation. If your booking dosen't show up here immediatly, please come back later.";
+  next();
+};
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from collection
@@ -12,75 +20,78 @@ exports.getOverview = catchAsync(async (req, res) => {
   // 3) Render that template using tour data from
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours
+    tours,
   });
 });
 
-exports.getTour = catchAsync(async(req, res,next) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour (including reviews and guides)
-const tour = await Tour.findOne({slug: req.params.slug}).populate({
-  path: 'reviews',
-  fields:'name rating summary'});
-  
-  if(!tour){
-    return next(new AppError('There is no tour with that name.',404));
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'name rating summary',
+  });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
   }
   // 2) Build templates
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
-    tour
+    tour,
   });
 });
 
-exports.getLoginForm = catchAsync(async(req,res,next)=>{
-  res.status(200).set(
-    'Content-Security-Policy',
-    "connect-src 'self' http://127.0.0.1:3000/"
-).render('login',{
-    title:'Log into your account'
-  })
-})
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  res
+    .status(200)
+    .set('Content-Security-Policy', "connect-src 'self' http://127.0.0.1:3000/")
+    .render('login', {
+      title: 'Log into your account',
+    });
+});
 
-exports.getAccount = catchAsync(async(req,res,next)=>{
-  res.status(200).set(
-    'Content-Security-Policy',
-    "connect-src 'self' http://127.0.0.1:3000/"
-).render('account',{
-    title:'Your account'
-  })
-})
+exports.getAccount = catchAsync(async (req, res, next) => {
+  res
+    .status(200)
+    .set('Content-Security-Policy', "connect-src 'self' http://127.0.0.1:3000/")
+    .render('account', {
+      title: 'Your account',
+    });
+});
 
-exports.getMyTours = catchAsync(async(req,res,next)=>{
-
+exports.getMyTours = catchAsync(async (req, res, next) => {
   // 1) Find all bookings
-  const bookings = await Booking.find({user:req.user.id});
+  const bookings = await Booking.find({ user: req.user.id });
   // 2) Find all tours the user has booked
-  const tourIDs = bookings.map(el=> el.tour);
+  const tourIDs = bookings.map((el) => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
-  res.status(200).render('overview',{
+  res.status(200).render('overview', {
     title: 'My Tours',
-    tours
+    tours,
   });
-})
+});
 
-exports.updateUserData = (req,res,next)=>{
-  console.log('UPDATING USER',req.body)
-}
+exports.updateUserData = (req, res, next) => {
+  console.log('UPDATING USER', req.body);
+};
 
-exports.updateUserData = catchAsync(async(req,res,next)=>{
-  const updatedUser = await User.findByIdAndUpdate(req.user.id,{
-    name:req.body.name,
-    email:req.body.email
-  },
-  {
-    new:true,
-    runValidators:true
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
   });
-
-  res.status(200).render('account',{
-    title:'Your account',
-    user: updatedUser
-  })
-})
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,74 +1,78 @@
-/*eslint-disable*/ 
+/*eslint-disable*/
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import '@swc/helpers'
+import '@swc/helpers';
 
-import {displayMap} from './mapbox';
-import {login,logout} from './login';
-import {updateSettings} from './updateSettings';
+import { displayMap } from './mapbox';
+import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
+import { showAlert } from './alerts';
 //---------------------------------------------------------------------------------------------
 import { bookTour } from './stripe';
 //---------------------------------------------------------------------------------------------
 
 // DOM ELEMENTS
-const mapBox =document.getElementById('map');
+const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el---logout');
 const updateDataForm = document.querySelector('.form-user-data');
-const userPasswordForm = document.querySelector('.form-user-password')
+const userPasswordForm = document.querySelector('.form-user-password');
 //---------------------------------------------------------------------------------------------
 const bookBtn = document.getElementById('book-tour');
 //---------------------------------------------------------------------------------------------
 
-
 // DELEGATION
 
-if(mapBox){
-    const locations = JSON.parse(mapBox.dataset.locations);
-    displayMap(locations);
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
 }
 
 if (loginForm)
-    loginForm.addEventListener('submit',e => {
-        e.preventDefault();
-        const email= document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        login(email,password);
-    });
-if (logOutBtn) logOutBtn.addEventListener('click',logout);
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if(updateDataForm)
-    updateDataForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const form = new FormData();
-        form.append('name',document.getElementById('name').value);
-        form.append('email',document.getElementById('email').value);
-        form.append('photo',document.getElementById('photo').files[0]);
-        // console.log(form);
+if (updateDataForm)
+  updateDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    // console.log(form);
 
-        updateSettings(form,'data');
-    })
-if(userPasswordForm)
-    userPasswordForm.addEventListener('submit',async e =>{    
-        e.preventDefault();
-        document.querySelector('btn--save-password').textContent='Updating...';
-        const passwordCurrent = document.getElementById('password-current').value;
-        const password = document.getElementById('password').value;
-        const passwordConfirm = document.getElementById('password-confirm').value;
-        await updateSettings({passwordCurrent,password,passwordConfirm},'password');
+    updateSettings(form, 'data');
+  });
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
 
-        document.querySelector('btn--save-password').textContent='Save password';
-        document.getElementById('password-current').value='';
-        document.getElementById('password').value='';
-        document.getElementById('password-confirm').value='';
-})
+    document.querySelector('btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
 
 if (bookBtn)
-    bookBtn.addEventListener('click', e => {
-      e.target.textContent = 'Processing...';
-      const { tourId } = e.target.dataset;
+  bookBtn.addEventListener('click', (e) => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
     //   console.log(tourId);
-      bookTour(tourId);
-    });
-  
-  
+    bookTour(tourId);
+  });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
